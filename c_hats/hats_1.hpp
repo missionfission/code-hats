36460f6f202777d5d56ea29ede4c17646e52b403
Khushal Sethi <iaskhushal@gmail.com>
2018-10-16 06:24:11 +0530

    all final results

--- c_hats/hats_1.hpp
+++ c_hats/hats_1.hpp
@@ -8,22 +8,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ap_int.h"
-
- typedef int addr_t;
- typedef int int_t;
- typedef int  tm_t;
- typedef float comp_t;
- typedef float hist_t;
-//typedef ap_int<14> addr_t;
-//typedef ap_int<14> int_t;
-//typedef int tm_t;
-//typedef ap_fixed<14,2,AP_RND_CONV,AP_SAT> comp_t;
-//typedef ap_fixed<14,2,AP_RND_CONV,AP_SAT>  hist_t;
+#include "ap_fixed.h"
+//
+//typedef ap_int<10> addr_t;
+//typedef ap_int<18> tm_t;
+//typedef ap_int<12> int_t;
+typedef int addr_t;
+typedef int  tm_t;
+typedef int int_t;
+typedef float comp_t;
+typedef float hist_t;
+typedef float sum_t;
+//typedef ap_fixed<24,12> comp_t;
+//typedef ap_int<12>  hist_t;
+//typedef ap_fixed<24,12>  sum_t;
 using namespace std;
 struct aer{
 addr_t x,y;
 tm_t t;
 bool p;
+bool reset;
 };
 const int P=8;
 const int p_factor=15;
@@ -32,7 +36,7 @@ const int N_PE=7;
 const int n = 5880;
 const long int tau=1000000;
 const int tot_cell=120/P;
-comp_t hats(aer events[] ,int_t n);
+sum_t hats(aer events[] ,int_t n);
 void computeHistogram(comp_t hist[rho][rho],aer event,aer memory[],int_t size);
-comp_t svm(comp_t hist[rho][rho], int_t offset);
+sum_t svm(comp_t hist[rho][rho], int_t offset);
 #endif 
--- c_hats/main.cpp
+++ c_hats/main.cpp
@@ -3,81 +3,115 @@
 #include <string.h>
 #include "hats_1.hpp"
 #include <sstream>
+#include <fstream>
 #include <dirent.h>
 #include <iostream>
+#include <string>
 #include "time.h"
 // #include <hls_opencv.h>
 // using namespace cv;
+sum_t computetest(aer data_test[],int size){
 
-comp_t computeHATS(aer data[],int_t size){
-comp_t weights[n];
-double w_temp[n];
-char *filename= "weights.bin";
-FILE *infile = fopen(filename, "rb");
-if (!infile) {
- printf("ERROR: File %s could not be opened!\n", filename);
- exit(-1);
+sum_t result=0;
+result+=hats(data_test,size);
+return result;
 }
-fread(w_temp, sizeof(double),n, infile);
-fclose(infile);
-for(int i=0;i<n;i++)
-{  
-weights[i]=comp_t(w_temp[i]);
-}
-int_t q_size[P]={0};
-aer data_q[P][1500];
-for(int_t j=0;j<size;j++)
-{
-int cell_id= ((data[j].x)/10)*10+(data[j].y)/10;
-if((cell_id/15)%2==0)
-{int k = data[j].x-(cell_id/15)*15;
-data[j].y=(data[j].y+(k/10)*100);}
-else
-{int k = data[j].x-(((cell_id/15)*15)/10)*10;
-data[j].y=(data[j].y+(k/10)*100-50);}
-data[j].x=data[j].x%10;
-data_q[cell_id/p_factor][q_size[cell_id/p_factor]]=data[j];
-q_size[cell_id/p_factor]++;
-}
+sum_t computeHATS(aer data_q[][6000],int_t q_size[],int size){
+//comp_t weights[n];
+//double w_temp[n];
+//char *filename= "weights.bin";
+//FILE *infile = fopen(filename, "rb");
+//if (!infile) {
+// printf("ERROR: File %s could not be opened!\n", filename);
+// exit(-1);
+//}
+//fread(w_temp, sizeof(double),n, infile);
+//fclose(infile);
+//for(int i=0;i<n;i++)
+//{
+//weights[i]=comp_t(w_temp[i]);
+//}
 
-comp_t result=0;
-for(int_t i=0;i<P;i++)
-{ 	comp_t *ptr=weights+i*rho*rho*tot_cell;
-	result+=hats(data_q[i], q_size[i]);
+sum_t result=0;
+//for(int i=0;i<P;i++)
+//	{
+//	result+=hats(data_q[i], q_size[i]);
+//	}
 
-}
+
 return result;
 }
 
-void readdata(aer data[],int size)
+void readdata(aer data_test[],aer data_q[][6000],int_t q_size[],int size, string filename)
 {
-int data2[size][4];
-FILE *infile = fopen("input.bin", "rb");
+int data[size][4];
+FILE *infile = fopen(filename.c_str(), "rb");
   if (!infile) {
     printf("ERROR: File could not be opened!\n");
     exit(-1);
   }
-  fread(data2, 4*sizeof(int)*size, 1, infile);
+  fread(data, 4*sizeof(int)*size, 1, infile);
   fclose(infile);
-for(int i=0;i<size;i++)
+for(int j=0;j<size;j++)
 {
-data[i].x=data2[i][0];
-data[i].y=data2[i][1];
-data[i].p=data2[i][2];
-data[i].t=data2[i][3];
-}}
-
+//int cell_id= ((data[j][0])/10)*10+(data[j][1])/10;
+//if((cell_id/15)%2==0)
+//{int k = data[j][0]-(cell_id/15)*15;
+//data[j][1]=(data[j][1]+(k/10)*100);}
+//else
+//{int k = data[j][0]-(((cell_id/15)*15)/10)*10;
+//data[j][1]=(data[j][1]+(k/10)*100-50);}
+//data[j][0]=data[j][0]%10;
+//data_q[cell_id/p_factor][q_size[cell_id/p_factor]].x=data[j][0];
+//data_q[cell_id/p_factor][q_size[cell_id/p_factor]].y=data[j][1];
+////data_q[cell_id/p_factor][q_size[cell_id/p_factor]].t=data[j][3];
+//data_q[cell_id/p_factor][q_size[cell_id/p_factor]].p=data[j][2];
+data_test[j].x=data[j][0];
+data_test[j].y=data[j][1];
+data_test[j].p=data[j][2];
+//data_test[j].t=data[j][3];
+//q_size[cell_id/p_factor]++;
+}
+}
+long int printSize(const std::string& address) {
+  std::fstream motd(address.c_str(), std::ios::binary|std::ios::in|std::ios::ate);
+  if(motd) {
+    std::fstream::pos_type size = motd.tellg();
+//    std::cout << address << " " << size << "\n";
+    return size;
+  } else {
+    perror(address.c_str());
+    return -1;
+  }
+}
 int main(int argc, char *argv[]) {
-const int size =3418;
-aer data[size];
-readdata(data,size);
-float result=computeHATS(data,size);
-cout<<result;
-if(abs(result-2.212)<0.22)
-{cout<<"correct";
-return 0;}
-else
-{cout<<"incorrect";
-return -1;
-}}
+int_t q_size[P];
+for(int i=0;i<P;i++)
+	q_size[i]=0;
+aer data_q[P][6000];
+aer data_test[8000];
+sum_t result;
+int i=0;
+float results[100];
+//std::string filename="files/input0.bin";
+while(i<100)
+{
+if(i!=21)
+{
+string s;
+std::stringstream out;
+out << i;
+s = out.str();
+std::string filename=(std::string)"files/input"+s+".bin";
+int size=printSize(filename)/4/sizeof(int);
+cout<<filename<<endl;
+readdata(data_test,data_q,q_size,size,filename);
+results[i]=computetest(data_test,size);
+cout<<results[i]<<endl;
+//computeHATS(data_q,q_size,size);
+}
+i++;
+}
+return 0;
+}
 
